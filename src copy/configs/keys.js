1a570c2025d6ccd9975f319181615eaad47c703b
yoonhee-jung <bcvcx0011@gmail.com>
2025-09-18 15:07:22 +0900

    250918 pwa 적용

--- src copy/configs/keys.js
+++
@@ -1,4 +0,0 @@
-export const KEY_LOCALSTORAGE_FESTIVAL_LIST = 'festivalList'
-export const KEY_LOCALSTORAGE_FESTIVAL_PAGE = 'festivalPage'
-export const KEY_LOCALSTORAGE_FESTIVAL_FLG = 'festivalFlg'
-export const KEY_LOCALSTORAGE_CLEAR_DATE = 'clearDate'
--- src copy/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import festivalReducer from './slices/festivalSlice.js';
-import festivalShowReducer from './slices/festivalShowSlice.js'
-export default configureStore({
-  reducer: {
-    festival: festivalReducer,
-    festivalShow: festivalShowReducer,
-  }
-});
