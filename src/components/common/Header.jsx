356c06f3c86ab9c48e396d1f0b57069f891c20e3
yoonhee-jung <bcvcx0011@gmail.com>
2025-09-19 18:10:43 +0900

    250919 header문제

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
 
 function Header() {
+  //const header = //classname정해서 addselector? 써서 &&으로 메인화면에만 header 안 보이게 해 보기
+
   return (
     <>
-      <h1>Green Travel</h1>
+      { if && <h1>Green Travel</h1>}
     </>
   )
 }
